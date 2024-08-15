6387b87508619ec1a035163c71aba02d5bfa5b74
Vedad Herceglija <Vedad.Herceglija@comtrade.com>
2024-08-15 16:32:49 +0200

    initialCommit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ColourAPI.Models;
+
+namespace ColourAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ColourContext _context;
+
+        public ValuesController(ColourContext context)
+        {
+            _context = context;
+        }
+    
+        [HttpGet]
+        public ActionResult<IEnumerable<Colour>> GetColourItems()
+        {
+            return _context.ColourItems;
+        }
+
+
+        // GET api/values
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get()
+        // {
+        //     return new string[] { "value1", "value2" };
+        // }
+
+    }
+}
